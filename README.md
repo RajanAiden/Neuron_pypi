6854eb6c992e3167aae7e815c3a033de865e54eb
RajanAiden <aidenrichard12.ar@gmail.com>
2021-10-06 06:19:01 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Neuron_pypi
+
+## How to use this
+
+```python
+from oneNeuron.perceptron import Perceptron
+
+## get X and y and then use below commands
+model = Perceptron(eta=eta, epochs=epochs)
+model.fit(X, y)
+```
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "RajanAiden"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="aidenrichad12.ar@gmail.com.com",
     description="It's an Implementation of Perceptron",
